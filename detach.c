c2ec0f85c3ad8cc1a45c56f449cbdf29d113e53e
Vladislav Punko <iam.vlad.punko@gmail.com>
2025-09-24 22:48:34 +0200

    Handle standard streams more naturally in daemon launcher

--- detach.c
+++ detach.c
@@ -1,20 +1,62 @@
 // Copyright 2020 (c) Vladislav Punko <iam.vlad.punko@gmail.com>
 
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char **argv) {
-  // Create a new process.
-  if (fork()) return 0;
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        perror("insufficient number of arguments");
 
-  close(0);
-  close(1);
-  close(2);
+        return EXIT_FAILURE;
+    }
 
-  // Create a new session.
-  setsid();
+    // Create a new process.
+    pid_t pid = fork();
+    if (pid > 0) {
+        return EXIT_SUCCESS;
+    } else if (pid < 0) {
+        perror("fork");
 
-  // Start a program in a new session on the current operating system.
-  execvp(argv[1], &argv[1]);
+        return EXIT_FAILURE;
+    }
 
-  return -1;
+    // Create a new session.
+    setsid();
+
+    // New stdin.
+    int fd_stdin = open("/dev/null", O_RDONLY);
+    if (fd_stdin < 0) {
+        perror("stdin");
+
+        return EXIT_FAILURE;
+    }
+    dup2(fd_stdin, STDIN_FILENO);
+    if (fd_stdin > STDIN_FILENO) {
+        close(fd_stdin);
+    }
+
+    // New stdout.
+    FILE* tmp_out = tmpfile();
+    if (!tmp_out) {
+        perror("stdout");
+
+        return EXIT_FAILURE;
+    }
+    dup2(fileno(tmp_out), STDOUT_FILENO);
+
+    // New stderr.
+    FILE* tmp_err = tmpfile();
+    if (!tmp_err) {
+        perror("stderr");
+
+        return EXIT_FAILURE;
+    }
+    dup2(fileno(tmp_err), STDERR_FILENO);
+
+    // Start a program in a new session on the current operating system.
+    execvp(argv[1], &argv[1]);
+
+    return EXIT_FAILURE;
 }
