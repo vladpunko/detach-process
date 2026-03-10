ed05320c132d702a1a25a37b2041a6244ba0bc54
Vladislav Punko <iam.vlad.punko@gmail.com>
2026-03-10 23:00:35 +0100

    Set install defaults to /usr/local/bin and /usr/share/man

--- install.sh
+++ install.sh
@@ -30,9 +30,11 @@ git clone --depth=1 --branch=master -- https://github.com/vladpunko/detach-proce
 gcc -Wall -Wextra -Werror -Wno-unused-parameter "${workdir}/detach.c" -o "${workdir}/detach"
 
 # Step -- 3.
-install -m 755 "${workdir}/detach" /usr/local/bin/detach
+bindir="${BINDIR:-/usr/local/bin}"
+install -d -m 755 "${bindir}"
+install -m 755 "${workdir}/detach" "${bindir}/detach"
 
 # Step -- 4.
-mandir="${MANDIR:-/usr/local/share/man}"
+mandir="${MANDIR:-/usr/share/man}"
 install -d -m 755 "${mandir}/man1"
 install -m 644 "${workdir}/detach.1" "${mandir}/man1/detach.1"
